427006db43cec39203d51ed22f036c261e297139
kjh <kjhied@gmail.com>
2023-06-19 17:28:52 +0900

    provider 오류 수정

--- Providers/AuthServiceProvider.php
+++ Providers/AuthServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace Modules\Qna\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Modules\Qna\Entities\Qna;
--- Providers/QnaServiceProvider.php
+++ Providers/QnaServiceProvider.php
@@ -37,6 +37,8 @@ class QnaServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->register(RouteServiceProvider::class);
+        $this->app->register(AuthServiceProvider::class);
+        // $this->app->register(EventServiceProvider::class);
     }
 
     /**
